c1105c6cd7a78eb423c5b783bf20b564ba14119d
roboter5123 <pascal.roboter@hotmail.com>
2023-02-21 09:15:40 +0100

    Added a page for the chat game

--- frontend/src/gamescreen/Chat.jsx
+++ frontend/src/gamescreen/Chat.jsx
@@ -1,9 +1,48 @@
 import {Component} from "react";
+import {Client} from '@stomp/stompjs';
+import websocket from 'websocket';
+
+Object.assign(global, { WebSocket: websocket.w3cwebsocket });
+
+Object.assign(global, {WebSocket});
+
 
 export class Chat extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {connection:null}
+    }
+
     render() {
+
         return (
-            <></>
+            <div></div>
         );
+    }
+
+    componentDidMount() {
+
+        this.initializeChat()
+    }
+
+    initializeChat() {
+
+        let sessionCode = this.props.session.code
+        const client = new Client({
+
+            brokerURL: "ws://localhost:8080/stomp-endpoint",
+            debug: (str) => console.log(str)
+        })
+
+        client.onConnect = subscribe
+        client.activate()
+        this.state.connection = client
+
+
+        function subscribe(){
+
+            client.subscribe("/topic/abc",(msg) => console.log(msg))
+        }
     }
 }
