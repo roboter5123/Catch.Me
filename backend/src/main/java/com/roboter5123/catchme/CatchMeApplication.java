fc409ecbfd56f59f0a4b642abdfde76a92af4e5d
roboter5123 <pascal.roboter@hotmail.com>
2023-02-23 12:39:54 +0100

    Configured cors in backend

--- backend/src/main/java/com/roboter5123/catchme/CatchMeApplication.java
+++ backend/src/main/java/com/roboter5123/catchme/CatchMeApplication.java
@@ -1,6 +1,8 @@
 package com.roboter5123.catchme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CatchMeApplication {
@@ -8,6 +10,15 @@ public class CatchMeApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(CatchMeApplication.class, args);
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig").allowedOrigins("*");
+            }
+        };
     }
 
 }
